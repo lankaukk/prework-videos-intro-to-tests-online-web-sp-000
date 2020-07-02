815b0d37011d8af00b49eedd771c01204c742158
McKayla Lankau <lankaukk@yahoo.com>
2020-07-02 00:52:43 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 RSpec.configure do |config|
   # Adds color configuration
   config.failure_color = :magenta # or whateer color you like
-  config.success_color = :rgba(175,253,)
+  config.success_color = :rgba(175,253,220,99)
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
